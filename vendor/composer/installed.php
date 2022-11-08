5f2587f07d11f76b724124285f593a325d9db690
Andy Fragen <andy@thefragens.com>
2022-11-08 12:53:07 -0800

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => 'afragen/git-updater-gitlab',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
+        'reference' => 'dde7154d0e8249ac662a6fd2cd01bce37d1d4056',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'd0e13b05eb31dfc97defc3f091c8591aa95c3ced',
-        'name' => 'afragen/git-updater-gitlab',
         'dev' => false,
     ),
     'versions' => array(
         'afragen/git-updater-gitlab' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
+            'reference' => 'dde7154d0e8249ac662a6fd2cd01bce37d1d4056',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'd0e13b05eb31dfc97defc3f091c8591aa95c3ced',
             'dev_requirement' => false,
         ),
     ),
